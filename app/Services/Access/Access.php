35ba754b1288a5831bd5faed5922ec931d2cd4d5
Rachmat Aditiya <raditiya@me.com>
2016-03-31 15:35:32 +0700

    test

--- app/Services/Access/Access.php
+++ app/Services/Access/Access.php
@@ -46,7 +46,7 @@ class Access
         $path = "C:\inetpub\wwwroot\hms";
         //$path = "/Users/Shared/hmsreport"; 
         chdir($path);
-        exec("echo test", $output);
+        exec("c:\\Program Files\\Git\\bin\\git", $output);
         for($i=0;$i<sizeof($output);$i++)
         {
             echo($output[$i]);
